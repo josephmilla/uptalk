989bd819d68d6ffc424d78d2aae75dcbb3c9f3c1
Joseph Milla <jjtmilla@gmail.com>
2015-04-19 08:12:25 -0500

    Fix chatbot controller

--- uptalk/www/js/controllers.js
+++ uptalk/www/js/controllers.js
@@ -20,6 +20,11 @@ angular.module('chatRoom.controllers', [])
     $scope.toggleSideMenu();
   };
 
+  $scope.goToChatbot = function() {
+    $location.path('/chatbot');
+    $scope.toggleSideMenu();
+  };
+
   $scope.goToChallenge = function() {
     $location.path('/challenge');
     $scope.toggleSideMenu();
@@ -104,27 +109,27 @@ angular.module('chatRoom.controllers', [])
 
 .controller('ChallengeCtrl', function($scope) {
 	$scope.baseArray = new Array("Do you like unicorns?","Don't tell me Santa Claus doesn't exist","Don't tell her. He is my boyfriend too!","I think it's the big belly. Everyone seems to want one.");
-	$scope.replyArrays= new Array(new Array("How can you like things that don't exist? So No.","Ok, I tell your sister's boyfriend to tell you.","Ah, damn Santa Claus is a such a player","It also serves as a floatation device in case of an emergency"),new Array("Who doesn't like unicorns?","Santa doesn't exist, the tooth fairy doesn't exist, and the dinosaurs are dead. Time to face the hard facts.","You have been known to get around","So chubby chasers like to come after you eh?"), new Array("Yes, but don't tell anyone","He is still real in my heart","Who isn't dating him?","A little extra cushion for the pushing"));	
+	$scope.replyArrays= new Array(new Array("How can you like things that don't exist? So No.","Ok, I tell your sister's boyfriend to tell you.","Ah, damn Santa Claus is a such a player","It also serves as a floatation device in case of an emergency"),new Array("Who doesn't like unicorns?","Santa doesn't exist, the tooth fairy doesn't exist, and the dinosaurs are dead. Time to face the hard facts.","You have been known to get around","So chubby chasers like to come after you eh?"), new Array("Yes, but don't tell anyone","He is still real in my heart","Who isn't dating him?","A little extra cushion for the pushing"));
 	$scope.right = 0;
 	$scope.cur=0;
 	$scope.newMessage = "";
 	$scope.messages = [];
 	$scope.username = 'User' + Math.floor(Math.random() * 501);
-	
+
 	for(var i=0; i<$scope.baseArray.length; i++){
 		 $scope.messages.push({
 			created_by: "mrBean",
 			content: $scope.baseArray[i],
 			created_at: new Date()
 		});
-		
+
 		$scope.messages.push({
 		   created_by: "mrHat",
 		   content: $scope.replyArrays[$scope.cur][i],
 		   created_at: new Date()
 		});
 	}
-	
+
 	$scope.nextConvo = function() {
 		$scope.cur++;
 		if($scope.cur>2){
@@ -137,7 +142,7 @@ angular.module('chatRoom.controllers', [])
 				content: $scope.baseArray[i],
 				created_at: new Date()
 			});
-			
+
 			$scope.messages.push({
 			   created_by: "mrHat",
 			   content: $scope.replyArrays[$scope.cur][i],
@@ -145,7 +150,7 @@ angular.module('chatRoom.controllers', [])
 			});
 		}
 	};
-   
+
 	$scope.submitAns = function() {
 		var correct = false;
 		if(correct){
@@ -221,6 +226,69 @@ mouseout : function(d) {
   //     $scope.$broadcast('scroll.refreshComplete');
   //   }, 500);
   // };
+  // var bot = {
+  // 	baseLang : "en-US",
+  // 	baseUrl : "http://www.botlibre.com/rest/botlibre/form-chat?instance=165&application=8821025394976658259&message=",
+  //
+  // 	getUrl : function(query) {
+  // 		var url = this.baseUrl + query;
+  // 		return url;
+  // 	},
+  //
+  // 	getMessage : function(xml) {
+  // 		var cut1 = xml.substr(0, xml.lastIndexOf("</message>"));
+  // 		var cut2 = cut1.substr(cut1.lastIndexOf("<message>") + "<message>".length);
+  // 		return cut2;
+  // 	},
+  //
+  // 	fetch : function(query, fn) {
+  // 		var url = this.getUrl(query);
+  // 		callUrl(url, true, fn);
+  // 	},
+  // };
+  //
+  // var go = {
+  // 	responses : [],
+  //
+  // 	beginChain : function(message) {
+  // 		go.sendChatBotRequest(message);
+  // 	},
+  //
+  // 	sendChatBotRequest : function(text) {
+  // 		bot.fetch(text, go.handleChatReceive);
+  // 	},
+  //
+  // 	handleChatReceive : function(res) {
+  // 		var text = bot.getMessage(res);
+  // 		if (text == "Hi Anonymous") text = "Hey! How are you?";
+  // 		go.responses.push(text);
+  // 	},
+  // };
+  //
+  // $scope.messages = responses;
+
+  $scope.newMessage = "";
+  $scope.messages = [];
+
+  var ref = new Firebase('https://chatroom-io.firebaseio.com/rooms/' + $routeParams.roomId);
+  var promise = angularFire(ref, $scope, "messages");
+
+  $scope.username = 'User' + Math.floor(Math.random() * 501);
+  $scope.submitAddMessage = function() {
+    $scope.messages.push({
+      created_by: this.username,
+      content: this.newMessage,
+      created_at: new Date()
+    });
+    this.newMessage = "";
+  };
+
+  $scope.onRefresh = function() {
+    var stop = $timeout(function() {
+      $scope.$broadcast('scroll.refreshComplete');
+    }, 500);
+  };
+
 })
 
 .controller('AboutCtrl', function($scope) {
@@ -233,9 +301,18 @@ mouseout : function(d) {
 })
 
 
+.controller('ChatbotCtrl', function($scope) {
+
+  // $scope.onRefresh = function() {
+  //   var stop = $timeout(function() {
+  //     $scope.$broadcast('scroll.refreshComplete');
+  //   }, 500);
+  // };
+})
+
 
 .controller('ChatStats1Ctrl', function($scope) {
-      
+
 $scope.chartConfig = {
         options: {
             chart: {
@@ -346,4 +423,3 @@ $scope.chartConfig = {
   //   }, 500);
   // };
 });
-
