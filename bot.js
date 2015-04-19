5a25b70872cd2c8413453d2a0d8f353f6b04a8eb
Joseph Milla <jjtmilla@gmail.com>
2015-04-19 02:02:22 -0500

    Add bot script

---
+++ bot.js
@@ -0,0 +1,38 @@
+var bot = {
+	baseLang : "en-US",
+	baseUrl : "http://www.botlibre.com/rest/botlibre/form-chat?instance=165&application=8821025394976658259&message=",
+
+	getUrl : function(query) {
+		var url = this.baseUrl + query;
+		return url;
+	},
+
+	getMessage : function(xml) {
+		var cut1 = xml.substr(0, xml.lastIndexOf("</message>"));
+		var cut2 = cut1.substr(cut1.lastIndexOf("<message>") + "<message>".length);
+		return cut2;
+	},
+
+	fetch : function(query, fn) {
+		var url = this.getUrl(query);
+		callUrl(url, true, fn);
+	},
+};
+
+var go = {
+	responses : [],
+
+	beginChain : function(message) {
+		go.sendChatBotRequest(message);
+	},
+
+	sendChatBotRequest : function(text) {
+		bot.fetch(text, go.handleChatReceive);
+	},
+
+	handleChatReceive : function(res) {
+		var text = bot.getMessage(res);
+		if (text == "Hi Anonymous") text = "Hey! How are you?";
+		go.responses.push(text);
+	},
+};
